46aa3698ba636466f811526a9009bc4c63c20d97
carolissima <156902923+carolissima@users.noreply.github.com>
2026-02-12 14:29:55 +0000

    Deploying to gh-pages from @ cogsci-mannheim/csi@9a3ccc8023e165633108b4d95db608c795f4b293 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -149,17 +149,6 @@ ninja.data = [{
             window.location.href = "/csi/blog/2024/pseudocode/";
           
         },
-      },{id: "post-a-post-with-vega-lite",
-        
-          title: "a post with vega lite",
-        
-        description: "this is what included vega lite code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/csi/blog/2024/vega-lite/";
-          
-        },
       },{id: "post-a-post-with-jupyter-notebook",
         
           title: "a post with jupyter notebook",
