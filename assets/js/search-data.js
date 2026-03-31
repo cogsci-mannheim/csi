24e2d875410a347d2ebb9dbe35fe01e74aff20f8
carolissima <156902923+carolissima@users.noreply.github.com>
2026-03-31 16:25:13 +0000

    Deploying to gh-pages from @ cogsci-mannheim/csi@edd4cff19efb58240b6c2d802976c3cd755d56d7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Here are the projects of the group",
           section: "Navigation",
           handler: () => {
             window.location.href = "/csi/projects/";
