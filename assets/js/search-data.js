2dadc9c199aaef4e361db31ee3cca55e4ca11041
carolissima <156902923+carolissima@users.noreply.github.com>
2026-03-31 16:37:08 +0000

    Deploying to gh-pages from @ cogsci-mannheim/csi@b0134d4c9b1224a977ece3198664bc19b1c745ab 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/csi/repositories/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/csi/cv/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "information about activities like guest lectures, talks, workshops",
