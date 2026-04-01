8eb5a0731c44796af0c59fb55a89b1fe1b8db84d
carolissima <156902923+carolissima@users.noreply.github.com>
2026-04-01 17:08:52 +0000

    Deploying to gh-pages from @ cogsci-mannheim/csi@557c3c1cb710788f1392310c7d081c2b5ceabba6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -163,8 +163,8 @@ ninja.data = [{
           description: "Representations in cognitive science",
           section: "Projects",handler: () => {
               window.location.href = "/csi/projects/1_project/";
-            },},{id: "teachings-data-science-fundamentals",
-          title: 'Data Science Fundamentals',
+            },},{id: "teachings-",
+          title: '',
           description: "",
           section: "Teachings",handler: () => {
               window.location.href = "/csi/teachings/data-science-fundamentals/";
