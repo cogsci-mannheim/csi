3d5078ae8b551330878a42a81129aac2a5c1447b
carolissima <156902923+carolissima@users.noreply.github.com>
2026-05-04 07:06:56 +0000

    Deploying to gh-pages from @ cogsci-mannheim/csi@81853efee64947d80850dba1875d60700e0ce7d2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -65,7 +65,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/csi/blog/";
               },
-            },{id: "post-meeting-8-working-on-representations",
+            },{id: "post-meeting-9-working-on-representations",
+        
+          title: "Meeting 9: working on representations",
+        
+        description: "Working on representations",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/csi/blog/2026/meeting9-reps/";
+          
+        },
+      },{id: "post-meeting-8-working-on-representations",
         
           title: "Meeting 8: working on representations",
         
